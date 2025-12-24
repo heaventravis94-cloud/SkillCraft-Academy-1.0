d654bfd6b1e35eb056c2fe2e2cb2d463babe82d6
Heaven Travis <appmanagement.ht@gmail.com>
2025-12-24 16:53:01 -0500

    Sync Android platform and updated web build for Codemagic

---
+++ android/app/src/main/java/com/skillcraft/academy/MainActivity.java
@@ -0,0 +1,5 @@
+package com.skillcraft.academy;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.skillcraft.academy',
+  appName: 'SkillCraft Academy',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
